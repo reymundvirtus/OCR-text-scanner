d357be9f00bcce8bb5bcb51687292d337c86daa2
reymundvirtus <reymundvirtus@outlook.com>
2022-03-28 18:30:50 +0800

    add scratch file

---
+++ lib/scratch.dart
@@ -0,0 +1,124 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class scratch extends StatefulWidget {
+  const scratch({Key? key}) : super(key: key);
+  // final String text;
+  // Translate(this.text);
+
+  @override
+  State<scratch> createState() => _scratchState();
+}
+
+enum AppState {
+  free,
+  picked,
+  cropped,
+}
+
+class _scratchState extends State<scratch> {
+  late AppState state;
+  File? imageFile;
+
+  @override
+  void initState() {
+    super.initState();
+    state = AppState.free;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Scratch"),
+      ),
+      body: Center(
+        child: imageFile != null ? Image.file(imageFile!) : Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrange,
+        onPressed: () {
+          if (state == AppState.free) {
+            _pickImage();
+          } else if (state == AppState.picked) {
+            _cropImage();
+          } else if (state == AppState.cropped) {
+            _clearImage();
+          }
+        },
+        child: _buildButtonIcon(),
+      ),
+    );
+  }
+
+  Widget _buildButtonIcon() {
+    if (state == AppState.free) {
+      return const Icon(Icons.add);
+    } else if (state == AppState.picked) {
+      return const Icon(Icons.crop);
+    } else if (state == AppState.cropped) {
+      return const Icon(Icons.clear);
+    } else {
+      return Container();
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    imageFile = pickedImage != null ? File(pickedImage.path) : null;
+    if (imageFile != null) {
+      setState(() {
+        state = AppState.picked;
+      });
+    }
+  }
+
+  Future<void> _cropImage() async {
+    File? croppedFile = await ImageCropper().cropImage(
+        sourcePath: imageFile!.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio16x9
+              ]
+            : [
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio5x3,
+                CropAspectRatioPreset.ratio5x4,
+                CropAspectRatioPreset.ratio7x5,
+                CropAspectRatioPreset.ratio16x9
+              ],
+        androidUiSettings: const AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: const IOSUiSettings(
+          title: 'Cropper',
+        ));
+    if (croppedFile != null) {
+      imageFile = croppedFile;
+      setState(() {
+        state = AppState.cropped;
+      });
+    }
+  }
+
+  void _clearImage() {
+    imageFile = null;
+    setState(() {
+      state = AppState.free;
+    });
+  }
+}
